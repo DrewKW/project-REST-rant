4edb646e66bbd83726053ef8a01ce477e90878dc
Andrew Wokabi <drewzote@Andrews-MBP.frontierlocal.net>
2022-06-14 16:24:42 -0700

    Implementing the Mongoose ODM

--- models/places.js
+++ models/places.js
@@ -1,42 +1,12 @@
-module.exports = [{
-	name: 'H-Thai-ML',
-	city: 'Seattle',
-	state: 'WA',
-	cuisines: 'Thai, Pan-Asian',
-	pic: '/images/blue-fin-restaurant.webp'
-    }, {
-	name: 'Coding Cat Cafe',
-	city: 'Phoenix',
-	state: 'AZ',
-	cuisines: 'Coffee, Bakery',
-	pic: '/images/ginger-cat-looks-laptop-screen-260nw-1893948898.webp'
-    }]
+const mongoose = require('mongoose')
 
-	router.put('/:id', (req, res) => {
-		let id = Number(req.params.id)
-		if (isNaN(id)) {
-			res.render('error404')
-		}
-		else if (!places[id]) {
-			res.render('error404')
-		}
-		else {
-			// Dig into req.body and make sure data is valid
-			if (!req.body.pic) {
-				// Default image if one is not provided
-				req.body.pic = '/images/image.jpg'
-			}
-			if (!req.body.city) {
-				req.body.city = 'Anytown'
-			}
-			if (!req.body.state) {
-				req.body.state = 'USA'
-			}
-	  
-			// Save the new data into places[id]
-			places[id] = req.body
-			res.redirect(`/places/${id}`)
-		}
-	  })
-	  
-	  
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
